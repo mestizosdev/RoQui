f5a1e892092cd21a4dbd558d7943b3d6d6303553
Jorge Luis <jorjoluiso@gmail.com>
2023-12-02 20:33:39 -0500

    [ADD] v_ele_invoices model

--- backend/RoQui/src/main/kotlin/dev/mestizos/roqui/invoice/model/Invoice.kt
+++ backend/RoQui/src/main/kotlin/dev/mestizos/roqui/invoice/model/Invoice.kt
@@ -1,4 +1,60 @@
 package dev.mestizos.roqui.invoice.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.Immutable
+import java.math.BigDecimal
+import java.util.*
+
+@Entity
+@Immutable
+@Table(name = "v_ele_invoices")
 class Invoice {
+
+    @Id
+    val id: Long? = null
+
+    @Column(name = "code")
+    val code: String? = null
+
+    @Column(name = "number")
+    val number: String? = null
+
+    @Column(name = "code_document")
+    val codeDocument: String? = null
+
+    @Column(name = "establishment")
+    val establishment: String? = null
+
+    @Column(name = "emission_point")
+    val emissionPoint: String? = null
+
+    @Column(name = "sequence")
+    val sequence: String? = null
+
+    @Column(name = "date", columnDefinition = "DATE")
+    val date: Date? = null
+
+    @Column(name = "total")
+    val total: BigDecimal? = null
+
+    @Column(name = "identification_type")
+    val identificationType: String? = null
+
+    @Column(name = "identification")
+    val identification: String? = null
+
+    @Column(name = "legal_name")
+    val legalName: String? = null
+
+    @Column(name = "address")
+    val address: String? = null
+
+    @Column(name = "delivery_note")
+    val deliveryNote: String? = null
+
+    @Column(name = "establishment_address")
+    val establishmentAddress: String? = null
 }
